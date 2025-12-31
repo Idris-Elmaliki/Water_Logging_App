116bf135cad0bb41794d69fd8775448b88b406b1
Idris <idris.elmaliki06@gmail.com>
2025-12-31 00:54:10 -0500

    I removed the wrong file...

---
+++ Water_Logging_App/app/src/main/java/com/example/water_logging_app/ui/MainAppFunc.kt
@@ -0,0 +1,19 @@
+package com.example.water_logging_app.ui
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
+import com.example.water_logging_app.ui.navigation.UiNavigationRoutes
+
+
+@Composable
+fun MainAppFunc(
+    modifier : Modifier = Modifier
+) {
+    val navController : NavHostController = rememberNavController()
+    UiNavigationRoutes(
+        navController = navController,
+        modifier = modifier
+    )
+}
